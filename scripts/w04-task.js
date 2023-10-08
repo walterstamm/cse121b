b84b39ade3462fb0ef644f35cbd455f202bc7738
wstammSeidor <walterstamm92@gmail.com>
2023-10-07 23:31:02 -0300

    week 4

---
+++ scripts/w04-task.js
@@ -0,0 +1,68 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    "name": "Walter Stamm",
+    "photo" : "images/walterstamm.png",
+    "favoriteFoods" : ["Pizza", "Pasta", "Steak"],
+    "hobbies" : ["Golf", "Fishing", "Hunting"],
+    "placesLived" : []
+};
+
+myProfile.placesLived.push({
+    "place": "Buenos Aires",
+    "length": "1 year"
+    }
+);
+
+
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push({
+    "place": "Cordoba",
+    "length": "1 year"
+    }
+);
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+document.querySelector("#name").textContent = myProfile.name;
+
+/* Photo with attributes */
+document.querySelector("#photo").setAttribute("src", myProfile.photo);
+
+
+/* Favorite Foods List*/
+
+
+myProfile.favoriteFoods.forEach(function(food) {
+    let li = document.createElement("li");
+    li.textContent = food;
+    document.querySelector("#favorite-foods").appendChild(li);
+});
+
+
+
+/* Hobbies List */
+
+myProfile.hobbies.forEach(function(hobby) {
+    let ul = document.createElement("ul");
+    ul.textContent = hobby;
+    document.querySelector("#hobbies").appendChild(ul);
+});
+
+/* Places Lived DataList */
+
+
+myProfile.placesLived.forEach(function(place) {
+    let dt = document.createElement("dt");
+    dt.textContent = place.place;
+    document.querySelector("#places-lived").appendChild(dt);
+
+    let dd = document.createElement("dd");
+    dd.textContent = place.length;
+    document.querySelector("#places-lived").appendChild(dd);
+});
