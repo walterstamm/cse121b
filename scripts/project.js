9c3e651b281307acb9844f041a6a71e888db5256
wstammSeidor <walterstamm92@gmail.com>
2023-10-25 00:08:28 -0300

    project

--- scripts/project.js
+++ scripts/project.js
@@ -1,4 +1,8 @@
-// Función para alternar la visualización de la lista de países y ciudades
+
+
+const cityDataElement = document.getElementById("data-weather");
+
+
 function toggleList() {
     let locations = document.getElementById("locations");
     if (locations.style.display === "none") {
@@ -9,18 +13,44 @@ function toggleList() {
 }
 
 // Función para buscar el clima según el país y la ciudad seleccionados
-function fetchWeather() {
-    let selectedCountry = document.getElementById("country").value;
+ const  fetchWeather = async()=> {
     let selectedCity = document.getElementById("city").value;
-    // Aquí puedes realizar la llamada a la API del clima con los valores de país y ciudad seleccionados
-    // Por ejemplo, puedes usar fetch() para realizar una solicitud a la API de clima
-    // Asegúrate de manejar la respuesta de la API y mostrar los datos correspondientes en tu aplicación
-    console.log("Buscando clima para: " + selectedCity + ", " + selectedCountry);
+    let cityName = document.getElementById("city")
+    cityName = cityName.options[cityName.selectedIndex].text;
+    console.log(cityName)
+    const response = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${selectedCity}?apikey=cXJo2LYmtQkfjEGtHnTS1x8rC8AoYYZN`);
+    let weatherData = await response.json();
+    console.log(weatherData[0])
+    console.log(weatherData[0].Temperature.Metric);
+    displayCityData(weatherData[0],cityName);
+
 }
 
-// Agrega aquí más funciones si las necesitas para tu proyecto
 
-// Ejemplo de llamada a función al cargar la página
-window.onload = function() {
-    // Puedes agregar aquí cualquier lógica que necesites al cargar la página
+const displayCityData = async (cityData,cityName) => {
+            cityDataElement.innerHTML = "";
+            // Create the article element
+            let article = document.createElement("article");
+
+            // Create the h3 element and set its text content
+            let h3 = document.createElement("h3");
+            h3.textContent = cityName;
+
+            let h4 = document.createElement("h4");
+            h4.textContent = 'Temperature: ' + cityData.Temperature.Metric.Value + '°C';
+
+            let h4text = document.createElement("h4");
+            h4text.textContent = 'Weather: ' + cityData.WeatherText;
+            
+            let moreInfo = document.createElement("a");
+            moreInfo.textContent = 'More info';
+            moreInfo.href = cityData.Link; 
+
+            article.appendChild(h3);
+            article.appendChild(h4);
+            article.appendChild(h4text);
+            article.appendChild(moreInfo);
+            
+           // Append the article element to the templesElement
+           cityDataElement.appendChild(article);
 };
